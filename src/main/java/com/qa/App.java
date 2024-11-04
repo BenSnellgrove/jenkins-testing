1934723eb947e8fc48811e3ce4eb02d45ecd25cf
Ben <94117159+BenSnellgrove@users.noreply.github.com>
2024-11-04 11:47:11 +0000

    Add files via upload

---
+++ src/main/java/com/qa/App.java
@@ -0,0 +1,27 @@
+package com.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(sayHello());
+        System.out.println(sayHelloToSomeone("Ben"));
+        System.out.println(sayGoodbye());
+    }
+
+    public static String sayHello(){
+        return "Hello World!";
+    }
+
+    public static String sayGoodbye(){
+        return "Goodbye World!";
+    }
+
+    public static String sayHelloToSomeone(String name){
+        return "Hello " + name ;
+    }
+}
